d711c7a35e3587b3d681fed6d7f5630ee145d4b5
fxanhkhoa <fxanhkhoa@gmail.com>
2018-04-10 17:48:35 +0700

    Add Scan QR_CODE

--- app/src/main/java/com/example/fxanhkhoa/supermarket_tracking_ble/MainActivity.java
+++ app/src/main/java/com/example/fxanhkhoa/supermarket_tracking_ble/MainActivity.java
@@ -1,22 +1,52 @@
 package com.example.fxanhkhoa.supermarket_tracking_ble;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
+    Button QR_BTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        QR_BTN = (Button) findViewById(R.id.QR_BTN);
+        QR_BTN.setOnClickListener(QR_BTN_Click);
     }
+
+    View.OnClickListener QR_BTN_Click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), QRCODE_SCAN.class);
+            startActivity(intent);
+        }
+    };
 }
