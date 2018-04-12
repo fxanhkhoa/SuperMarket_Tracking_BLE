1a850e00bf7f31ba48b82dd868545d9b837884e1
phucuongcttpcl <nguyenphucuong.4thvalley@gmail.com>
2018-04-12 23:27:37 +0700

    This is first commit, almost similar with Mapwize App

---
+++ app/build/generated/source/buildConfig/debug/io/mapwize/app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.mapwize.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.mapwize.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30003;
+  public static final String VERSION_NAME = "3.0.2";
+}
